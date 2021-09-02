a705ec2d60ad9f45d04eba7314fbb3288d21aff2
Documenter.jl <documenter@juliadocs.github.io>
2021-09-02 19:52:17 +0000

    build based on ae76d16

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TypeDB_tutorial","category":"page"},{"location":"#TypeDB_tutorial","page":"Home","title":"TypeDB_tutorial","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TypeDB_tutorial.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TypeDB_tutorial]","category":"page"}]
+}
